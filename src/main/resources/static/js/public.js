7bab96fb440989bf5181298fea56ead630de0cd5
llllllxy <34022230+llllllxy@users.noreply.github.com>
2024-09-19 15:08:39 +0800

    feat: 修复ajax cache的默认错误问题，默认应该是false不缓存

--- src/main/resources/static/js/public.js
+++ src/main/resources/static/js/public.js
@@ -91,7 +91,7 @@ const AjaxUtil = {
         options.type = 'GET';
         options.timeout = options.timeout || 5000; // 设置本地的请求超时时间（以毫秒计）
         options.async = options.async !== false; // 布尔值，表示请求是否异步处理。默认是 true
-        options.cache = options.cache !== true; // 布尔值，表示浏览器是否缓存被请求页面，默认是false
+        options.cache = options.cache === true; // 布尔值，表示浏览器是否缓存被请求页面，默认是false
         options.dataType = options.dataType || 'json';
         $.ajax({
             url: options.url,
@@ -123,7 +123,7 @@ const AjaxUtil = {
         options.type = 'POST';
         options.timeout = options.timeout || 5000;
         options.async = options.async !== false;
-        options.cache = options.cache !== true;
+        options.cache = options.cache === true;
         options.dataType = options.dataType || 'json';
         options.contentType = options.contentType || 'application/x-www-form-urlencoded';
         options.data = options.data || {};
@@ -159,7 +159,7 @@ const AjaxUtil = {
         options.type = 'POST';
         options.timeout = options.timeout || 5000;
         options.async = options.async !== false;
-        options.cache = options.cache !== true;
+        options.cache = options.cache === true;
         options.dataType = options.dataType || 'json';
         options.contentType = options.contentType || 'application/json';
         options.data = options.data || '';
@@ -194,7 +194,7 @@ const AjaxUtil = {
         options.type = 'POST';
         options.timeout = options.timeout || 5000;
         options.async = options.async !== false;
-        options.cache = options.cache !== true;
+        options.cache = options.cache === true;
         options.dataType = options.dataType || 'json';
         options.contentType = options.contentType || false;
         options.processData = options.processData || false;
